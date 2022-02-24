05e1f48db8905cec272d1300e465b3ec078ea7c1
lanceterrence <magsombol.lance@gmail.com>
2022-02-24 11:46:55 +0800

    feat: add to cart button unfinished

--- src/components/ActionsComponent.tsx
+++ src/components/ActionsComponent.tsx
@@ -1,5 +1,8 @@
 import React from "react"
-import { Box, Button, ButtonGroup } from "@chakra-ui/react"
+import { Box, Button, ButtonGroup, Text, Image } from "@chakra-ui/react"
+import IconCart from "../assets/icon-cart.svg"
+import IconMinus from "../assets/icon-minus.svg"
+import IconPlus from "../assets/icon-plus.svg"
 
 type ActionsComponentProps = {
     quantity: number,
@@ -17,11 +20,27 @@ const ActionsComponent: React.FC<ActionsComponentProps> = (
             display="flex"
             flexDirection="row"
         >
-            <ButtonGroup isAttached>
-                <Button onClick={() => setQuantity(quantity - 1)} disabled={!quantity}>-</Button>
-                <Button>{quantity}</Button>
-                <Button onClick={() => setQuantity(quantity + 1)}>+</Button>
+            <ButtonGroup isAttached marginRight="20px">
+                <Button height="100%" onClick={() => setQuantity(quantity - 1)} disabled={!quantity}>
+                    <Image src={IconMinus} alt="icon-minus" />
+                </Button>
+                <Button height="100%">{quantity}</Button>
+                <Button height="100%" onClick={() => setQuantity(quantity + 1)}>
+                    <Image src={IconPlus} alt="icon-plus" />
+                </Button>
             </ButtonGroup>
+            <Button
+                size={"lg"}
+                width="100%"
+                bgColor="customPrimary.orange"
+                color="white"
+                fontSize="15px"
+                _hover={{
+                    bgColor: "customPrimary.paleOrange"
+                }}
+            >
+                <Image src={IconCart} alt="icon-cart" marginRight="10px" /> Add to cart
+            </Button>
         </Box>
     )
 }
