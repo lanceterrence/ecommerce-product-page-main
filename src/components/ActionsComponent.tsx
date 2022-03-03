665b2f4c64595373605b7ef3bad8848aff8b06ad
Lance Magsombol <lance@oostvoort.com>
2022-03-03 15:17:12 +0800

    refactor: remove unused import

--- src/components/ActionsComponent.tsx
+++ src/components/ActionsComponent.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Box, Button, ButtonGroup, Text, Image } from "@chakra-ui/react"
+import { Box, Button, ButtonGroup, Image } from "@chakra-ui/react"
 import IconCart from "../assets/icon-cart.svg"
 import IconMinus from "../assets/icon-minus.svg"
 import IconPlus from "../assets/icon-plus.svg"
@@ -10,38 +10,38 @@ type ActionsComponentProps = {
 }
 
 const ActionsComponent: React.FC<ActionsComponentProps> = (
-    {
-        quantity,
-        setQuantity
-    }
+  {
+      quantity,
+      setQuantity
+  }
 ) => {
     return (
-        <Box
-            display="flex"
-            flexDirection="row"
-        >
-            <ButtonGroup isAttached marginRight="20px">
-                <Button height="100%" onClick={() => setQuantity(quantity - 1)} disabled={!quantity}>
-                    <Image src={IconMinus} alt="icon-minus" />
-                </Button>
-                <Button height="100%">{quantity}</Button>
-                <Button height="100%" onClick={() => setQuantity(quantity + 1)}>
-                    <Image src={IconPlus} alt="icon-plus" />
-                </Button>
-            </ButtonGroup>
-            <Button
-                size={"lg"}
-                width="100%"
-                bgColor="customPrimary.orange"
-                color="white"
-                fontSize="15px"
-                _hover={{
-                    bgColor: "customPrimary.paleOrange"
-                }}
-            >
-                <Image src={IconCart} alt="icon-cart" marginRight="10px" /> Add to cart
-            </Button>
-        </Box>
+      <Box
+        display="flex"
+        flexDirection="row"
+      >
+          <ButtonGroup isAttached marginRight="20px">
+              <Button height="100%" onClick={() => setQuantity(quantity - 1)} disabled={!quantity}>
+                  <Image src={IconMinus} alt="icon-minus" />
+              </Button>
+              <Button height="100%">{quantity}</Button>
+              <Button height="100%" onClick={() => setQuantity(quantity + 1)}>
+                  <Image src={IconPlus} alt="icon-plus" />
+              </Button>
+          </ButtonGroup>
+          <Button
+            size={"lg"}
+            width="100%"
+            bgColor="customPrimary.orange"
+            color="white"
+            fontSize="15px"
+            _hover={{
+                bgColor: "customPrimary.paleOrange"
+            }}
+          >
+              <Image src={IconCart} alt="icon-cart" marginRight="10px" /> Add to cart
+          </Button>
+      </Box>
     )
 }
 
